14033969d35d363702070b048f3578104b02977b
pdchinh <pdc1761.n@gmail.com>
2022-07-14 20:26:22 +0700

    aes encrypt message

--- client.py
+++ client.py
@@ -1,46 +1,45 @@
 import socket
 import threading
 import sys
+from AES_cipher import AESCipher
 
 from colorama import Fore, Back, Style
 
-# Choosing Nickname
+aes = AESCipher()
 nickname = input("Choose your nickname: ")
 
-# Connecting To Server
+# if len(sys.argv) != 3:
+#     print ("Correct usage: script IP address port number")
+#     exit()
+# host = str(sys.argv[1])
+# port = int(sys.argv[2])
+host = '127.0.0.1'
+port = 55555
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-if len(sys.argv) != 3:
-    print ("Correct usage: script IP address port number")
-    exit()
-host = str(sys.argv[1])
-port = int(sys.argv[2])
 client.connect((host, port))
-# Listening to Server and Sending Nickname
+
+
 def receive():
     while True:
         try:
-            # Receive Message From Server
-            # If 'NICK?' Send Nickname
-            message = client.recv(1024).decode('utf-8')
-            if message == 'WHAT IS YOUR NICK?':
-                client.send(nickname.encode('utf-8'))
+            message = aes.decrypt(client.recv(1024).decode('utf-8'))
+            if message == 'WHAT IS YOUR NAME?':
+                client.send(aes.encrypt(nickname).encode('utf-8'))
             else:
-                print(Fore.GREEN +message)
+                print(Fore.GREEN + message)
                 print(Style.RESET_ALL)
         except:
-            # Close Connection When Error
-            # print("An error occured!")
             client.close()
             sys.exit()
-# Sending Messages To Server
+
 def write():
     while True:
         message = '{}: {}'.format(nickname, input())
-        client.send(message.encode('utf-8'))
+        client.send(aes.encrypt(message).encode('utf-8'))
         if message.split(': ')[1]=='exit!':
             client.close()
             sys.exit()
-# Starting Threads For Listening And Writing
+
 receive_thread = threading.Thread(target=receive)
 receive_thread.start()
 
--- server.py
+++ server.py
@@ -1,72 +1,63 @@
 import socket
 import threading
+from AES_cipher import AESCipher
 
 
-
-# Connection Data
 host = '127.0.0.1'
 port = 55555
+clients = []
+nicknames = []
 
-# Sending Messages To All Connected Clients
+
 def broadcast(message,clientSend=None):
-    tmp = b'                         '
+    pad = '                         '
     for client in clients:
         if client != clientSend:
-            client.send(tmp+message)
+            client.send(aes.encrypt(pad + message).encode('utf-8'))
 
-# Handling Messages From Clients
+
 def handle(client):
     while True:
         try:
-            # Broadcasting Messages
-            message = client.recv(1024)
-            if message.decode('utf-8').split(': ')[1]=='exit!':
+            message = aes.decrypt(client.recv(1024).decode('utf-8'))
+            if message.split(': ')[1]=='exit!':
                 a=1/0
             broadcast(message,client)
         except:
-            # Removing And Closing Clients
-            # print('ERROR!!!')
             index = clients.index(client)
             client.close()
             clients.remove(client)
             
             nickname = nicknames[index]
-            broadcast('{} left!'.format(nickname).encode('utf-8'))
+            broadcast('{} left!'.format(nickname))
             print(nickname,' left!')
             nicknames.remove(nickname)
             break
 
-# Receiving / Listening Function
 def receive():
     while True:
-        # Accept Connection
+
         client, address = server.accept()
         print("Connected with {}".format(str(address)))
 
-        # Request And Store Nickname
-        client.send('WHAT IS YOUR NICK?'.encode('utf-8'))
-        nickname = client.recv(1024).decode('utf-8')
+
+        client.send(aes.encrypt('WHAT IS YOUR NAME?').encode('utf-8'))
+        nickname = aes.decrypt(client.recv(1024).decode('utf-8'))
 
         nicknames.append(nickname)
         clients.append(client)
 
-        # Print And Broadcast Nickname
-        print("Nickname is {}".format(nickname))
-        broadcast("{} joined!".format(nickname).encode('utf-8'))
-        client.send('Connected to server!'.encode('utf-8'))
+        print("Nickname is {}\n".format(nickname))
+        broadcast("{} joined!".format(nickname))
+        client.send(aes.encrypt('Connected to server!').encode('utf-8'))
 
-        # Start Handling Thread For Client
         thread = threading.Thread(target=handle, args=(client,))
         thread.start()
 
-# Starting Server
+aes = AESCipher()
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
 server.bind((host, port))
 server.listen()
-
-# Lists For Clients and Their Nicknames
-clients = []
-nicknames = []
-
 print('SERVER IS RUNNING....')
 receive()                
